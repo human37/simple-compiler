a61a603e46ab8bb88f4d0b65ebc709990f4f0db4
Ammon Taylor <ammonx9@gmail.com>
2021-04-25 15:40:13 -0600

    added some basic tests to INPUT.c

--- INPUT.c
+++ INPUT.c
@@ -1,19 +1,24 @@
 void main()
 {
-    // integers
+    // integers and math
     int sum;
-    sum = 35 + 400;
+    sum = (35 + 1) / (36 * 1) * (1 & 1);
+    cout << sum << endl;
+
     // cout statements
-    cout << endl << sum << 3838 << endl;
+    cout << endl << 0 << sum << 0 << endl;
+
     // plus equals & minus equals
     int test;
     test = 0;
-    cout << test;
+    cout << test << endl;
     test += 99;
-    cout << test;
+    cout << test << endl;
     test -= 99;
-    cout << test;
+    cout << test << endl;
+
     // while loops
+    int max;
     max = 10;
     while (max >= 0)
     {
